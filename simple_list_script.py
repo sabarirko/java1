a7edf4cd961b7a8153911edcd958ce0113e22765
sabarirko <59568865+sabarirko@users.noreply.github.com>
2025-05-20 08:13:44 +0530

    Add files via upload

---
+++ simple_list_script.py
@@ -0,0 +1,11 @@
+# A simple list in Python
+my_list = ['apple', 'banana', 'cherry']
+
+# Adding an item to the list
+my_list.append('orange')
+
+# Removing an item from the list
+my_list.remove('banana')
+
+# Printing the list
+print("Updated List:", my_list)
